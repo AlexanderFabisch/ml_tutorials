62f543e146f8a4fbd8267b12fff062eba2e1f62b
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-09-30 18:21:18 +0200

    Correct config

--- ipython_nbconvert_config.py
+++ ipython_nbconvert_config.py
@@ -1,5 +1,4 @@
 c = get_config()
 c.NbConvertApp.notebooks = ['*.ipynb']
 c.NbConvertApp.export_format = 'slides'
-#c.NbConvertApp.postprocessor_class = 'serve'
 c.Exporter.template_file = 'slides.tpl'
